0045860ededb32acf951b8d40ebfdf5cef33870b
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-09-05 21:26:13 +0100

    Refactoring

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -28,27 +28,39 @@ describe('Feature tests', function() {
     expect(thermostat.currentTemp()).toBe(10);
   });
 
-  it('has a power saving mode (PSM), which is on by default', function() {
-    expect(thermostat.isPSMOn()).toBe(true);
+  describe('it has a power saving mode', function() {
+    it('power saving mode (PSM) is on by default', function() {
+      expect(thermostat.isPSMOn()).toBe(true);
+    });
+
+    it('can switch power saving mode (PSM) off', function() {
+      thermostat.switchOffPSM();
+      expect(thermostat.isPSMOn()).toEqual(false);
+    });
+
+    it('can switch power saving mode (PSM) on', function() {
+      thermostat.switchOnPSM();
+      expect(thermostat.isPSMOn()).toEqual(true);
+    });
   });
 
-  it('can switch power saving mode (PSM) off', function() {
-    thermostat.switchOffPSM();
-    expect(thermostat.isPSMOn()).toEqual(false);
-  });
-
-  it('can switch power saving mode (PSM) on', function() {
-    thermostat.switchOnPSM();
-    expect(thermostat.isPSMOn()).toEqual(true);
-  });
 
   describe('when power saving mode (PSM) is on', function() {
     it('has a max temp of 25', function() {
       for (var n = 0; n < 6; n++ ) {
         thermostat.up();
       }
-      console.log(thermostat.isMaxTemp())
       expect(thermostat.currentTemp()).toEqual(25);
+    });
+  });
+
+  describe('when power saving mode (PSM) is off', function() {
+    it('has a max temp of 32', function() {
+      thermostat.switchOffPSM();
+      for (var n = 0; n < 13; n++ ) {
+        thermostat.up();
+      }
+      expect(thermostat.currentTemp()).toEqual(32);
     });
   });
 });
