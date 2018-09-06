87d087892f5b6a760bd2914d8cbd1cefd89f2b61
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-09-06 15:11:02 +0100

    Adds jquery and functionality

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,34 @@
+$(document).ready(function() {
+
+  var thermostat = new Thermostat();
+  $("#temperature").text(thermostat.temperature)
+
+  $("#up").click(function() {
+    thermostat.up()
+    $("#temperature").text(thermostat.temperature)
+  });
+
+  $("#down").click(function() {
+    thermostat.down()
+    $("#temperature").text(thermostat.temperature)
+  });
+
+  $("#reset").click(function() {
+    thermostat.resetTemp()
+    $("#temperature").text(thermostat.temperature)
+  });
+
+  $("#toggle").click(function() {
+    if (thermostat.isPSMOn()) {
+      thermostat.switchOffPSM()
+    } else {
+      thermostat.switchOnPSM()
+      thermostat.temperature = thermostat.MAX_TEMP_PSM_ON
+    }
+    $("#temperature").text(thermostat.temperature)
+  });
+});
+
 function Thermostat() {
   this.DEFAULT_TEMP = 20;
   this.MIN_TEMP = 10;
